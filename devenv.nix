76cfd804fc67eed76f3cdf134cfae24dbb648999
Dmitry Mineev <dmitry.mineev@gmail.com>
2022-11-29 20:55:21 +0100

    Test "Ignoring [gemname] because its extensions are not built"

--- devenv.nix
+++ devenv.nix
@@ -1,7 +1,7 @@
 { pkgs, nixpkgs-ruby, ... }:
 
 {
-  packages = [ ];
+  packages = [ pkgs.pkg-config pkgs.openssl ];
   languages.ruby.enable = true;
   languages.ruby.package = nixpkgs-ruby.lib.mkRuby { inherit pkgs; rubyVersion = "3.0.0"; };  
 }
